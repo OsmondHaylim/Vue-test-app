e25de262eede90b6d7836c3625a2b94e608c6c20
Osmond Haylim <osmond20001@mail.unpad.ac.id>
2024-10-28 17:34:04 +0700

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
   import Navbar from '@/components/navbar.vue';
   import Hero from '@/components/hero.vue';
+  import HomeCard from '@/components/homeCard.vue';
+  import JobLists from '@/components/jobLists.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCard />
+  <JobLists />
 </template>
---
+++ src/components/jobLists.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import data from '@/jobs.json';
+    import { ref } from 'vue';
+    import JobCard from '@/components/jobCard.vue';
+    const jobs = ref(data);
+
+</script>
+
+<template>
+    <section class="bg-green-50 px-4 py-10">
+      <div class="container-xl lg:container m-auto">
+        <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <!-- Job Listing 1 -->
+          <JobCard v-for="eachJob in jobs" :key="eachJob.id" :job="eachJob" />
+        </div>
+      </div>
+    </section>
+</template>
